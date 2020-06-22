47504e99bcf210ef4812b6db70e7cf9c0de563ae
Muslum Dag <cybersecurity@muslumdag.com>
2020-06-22 03:27:19 +0300

    Add files via upload

---
+++ subdomain.py
@@ -0,0 +1,20 @@
+import requests
+
+
+domain = input("domain gir:")
+file = open ("subdomain.txt","r")
+
+content = file.read()
+subdomains = content.splitlines()
+
+for subdomain in subdomains:
+    url = f"http://{subdomain}.{domain}"
+
+    try :
+        requests.get(url)
+
+    except ConnectionError:
+        pass
+
+    else:
+        print("(+) bulundu : ",url)
